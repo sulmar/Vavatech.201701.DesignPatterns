68aea906d8a92f49eed8b591b12b745bea9b67e9
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2017-10-18 08:49:31 +0200

    Dodanie wzorca Visitor

--- Vavatech.DesignPatterns.BehavioralPatterns.Visitor/Program.cs
+++ Vavatech.DesignPatterns.BehavioralPatterns.Visitor/Program.cs
@@ -17,16 +17,23 @@ namespace Vavatech.DesignPatterns.BehavioralPatterns.Visitor
             document.Add(new Hiperlink("Github", "https://github.com/sulmar/Vavatech.201710.DesignPatterns"));
             document.Add(new PlainText("End."));
 
-            Console.WriteLine(document.ToHtml());
+            IVisitor visitor = new HtmlVisitor();
+
+            document.Accept(visitor);
+
+            Console.WriteLine(visitor.Output);
         }
     }
+
 
 
     abstract class DocumentPart
     {
         public string Text { get; set; }
 
-        public abstract string ToHtml();
+      //  public abstract string ToHtml();
+
+        public abstract void Accept(IVisitor visitor);
     }
 
     class PlainText : DocumentPart
@@ -36,10 +43,15 @@ namespace Vavatech.DesignPatterns.BehavioralPatterns.Visitor
             this.Text = text;
         }
 
-        public override string ToHtml()
+        public override void Accept(IVisitor visitor)
         {
-            return this.Text;
+            visitor.Visit(this);
         }
+
+        //public override string ToHtml()
+        //{
+        //    return this.Text;
+        //}
     }
 
     class BoldText : DocumentPart
@@ -49,26 +61,36 @@ namespace Vavatech.DesignPatterns.BehavioralPatterns.Visitor
             this.Text = text;
         }
 
-        public override string ToHtml()
+        public override void Accept(IVisitor visitor)
         {
-            return $"<b>{this.Text}</b>";
+            visitor.Visit(this);
         }
+
+        //public override string ToHtml()
+        //{
+        //    return $"<b>{this.Text}</b>";
+        //}
     }
 
     class Hiperlink : DocumentPart
     {
-        private string url;
+        public string Url { get; private set; }
 
         public Hiperlink(string text, string url)
         {
             this.Text = text;
-            this.url = url;
+            this.Url = url;
         }
 
-        public override string ToHtml()
+        public override void Accept(IVisitor visitor)
         {
-            return $"<a href='{this.url}'>{this.Text}</a>";
+            visitor.Visit(this);
         }
+
+        //public override string ToHtml()
+        //{
+        //    return $"<a href='{this.Url}'>{this.Text}</a>";
+        //}
     }
 
     class Document
@@ -80,16 +102,62 @@ namespace Vavatech.DesignPatterns.BehavioralPatterns.Visitor
             parts.Add(part);
         }
 
-        public string ToHtml()
+        public void Accept(IVisitor visitor)
         {
-            string output = string.Empty;
-
             foreach (var part in parts)
             {
-                output += part.ToHtml();
+                part.Accept(visitor);
             }
+        }
 
-            return output;
+        //public string ToHtml()
+        //{
+        //    string output = string.Empty;
+
+        //    foreach (var part in parts)
+        //    {
+        //        output += part.ToHtml();
+        //    }
+
+        //    return output;
+        //}
+    }
+
+    interface IVisitor
+    {
+        void Visit(PlainText part);
+        void Visit(BoldText part);
+        void Visit(Hiperlink part);
+
+        string Output { get; }
+    }
+
+
+    class HtmlVisitor : IVisitor
+    {
+        private string output;
+        public string Output
+        {
+            get
+            {
+                return output;
+            }
+        }
+
+        public void Visit(PlainText part)
+        {
+            output += part.Text;
+        }
+
+        public void Visit(BoldText part)
+        {
+            output += $"<b>{part.Text}</b>";
+        }
+
+        public void Visit(Hiperlink part)
+        {
+            output += $"<a href='{part.Url}'>{part.Text}</a>";
         }
     }
+
 }
